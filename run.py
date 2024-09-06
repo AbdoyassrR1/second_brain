794519c359b416486160aa1a93611e45b0d12081
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-06 19:26:58 +0300

    updated

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
+#!/usr/bin/python3
 from app.app import create_app
+
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(
-        host="localhost",
-        debug=True
-    )
+    app.run(host="localhost", debug=True)
