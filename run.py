6466628cc34317b603b9daecfd3c391452a319c3
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-08 19:55:46 +0300

    app entry with error handlers for specific status codes

--- run.py
+++ run.py
@@ -1,9 +1,36 @@
 #!/usr/bin/python3
 from app.app import create_app
-
+from flask import jsonify
 
 app = create_app()
 
+@app.errorhandler(400)
+def bad_request_error(error):
+    return jsonify({
+        "status": "error: Bad Request",
+        "message": str(error.description)
+        }), 400
+
+@app.errorhandler(401)
+def unauthorized_error(error):
+    return jsonify({
+        "status": "error: Unauthorized",
+        "message": str(error.description)
+        }), 401
+
+@app.errorhandler(409)
+def conflict_error(error):
+    return jsonify({
+        "status": "error: Conflict",
+        "message": str(error.description)
+        }), 409
+
+@app.errorhandler(429)
+def ratelimit_error(error):
+    return {
+        "status": "error: Too Many Requests",
+        "message": "You have exceeded the maximum number of requests allowed. Please try again later."
+        }, 429
 
 if __name__ == "__main__":
     app.run(host="localhost", debug=True)
