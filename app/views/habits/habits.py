cbdad213e2399e26dd31704bbdbc6f97b9c7cdc4
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-11 22:40:17 +0300

    add create_habit route to create habits

--- app/views/habits/habits.py
+++ app/views/habits/habits.py
@@ -13,3 +13,35 @@ def get_habits():
     """ Get all habits related to the logged in User """
     habits = [habit.to_dict() for habit in current_user.habits]
     return jsonify(habits)
+
+
+@habits.route("/create_habit", methods=["POST"])
+@login_required
+def create_task():
+    """Create a new habit for the logged in user"""
+    data = request.form
+    # check if the habit exists for the current user
+    if Habit.query.filter_by(title=data["title"], user_id=current_user.id).first():
+        abort(409, description="this habit already exists for the current user")
+
+    # Check required fields
+    required_fields = ["title"]
+    for field in required_fields:
+        if field not in data:
+            abort(400, description=f"Missing {field}")
+
+
+    # Create new habit
+    new_habit = Habit(
+        title=data["title"],
+        description=data.get("description", ""),
+    )
+    new_habit.user_id = current_user.id
+    db.session.add(new_habit)
+    db.session.commit()
+
+    return jsonify({
+        "status": "success",
+        "message": "Habit Created Successfully",
+        "habit": new_habit.to_dict()
+    }), 201
