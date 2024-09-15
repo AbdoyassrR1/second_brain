bf45121ed699f6499a0f6a61add248f92e82647e
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-09-15 04:05:36 +0300

    add route for adding new transaction

--- app/views/finances/finances.py
+++ app/views/finances/finances.py
@@ -27,3 +27,54 @@ def get_transactions():
     transactions = [trans.to_dict() for trans in query.all()]
 
     return jsonify(transactions)
+
+
+@finances.route("/add_transaction", methods=["POST"])
+@login_required
+def add_transaction():
+    """ add new transaction for the logged in user """
+    data = request.form
+
+    # Check required fields
+    required_fields = ["title", "type", "amount"]
+    for field in required_fields:
+        if field not in data:
+            abort(400, description=f"Missing {field}")
+
+    # Validate and convert enum fields
+    try:
+        type = TransactionType[data["type"].upper()]
+        sub_category = data.get("sub_category", None)
+        if sub_category:
+            sub_category = TransactionSubCategory[sub_category.upper()]
+    except KeyError as e:
+        error = str(e).split("\'")[1]
+        abort(400, description=f"Invalid ENUM value for {error}")
+
+    # Validate and convert amount field
+    try:
+        amount = float(data["amount"])
+        if amount <= 0:
+            abort(400, description="Amount must be a positive number")
+    except ValueError:
+        abort(400, description="Invalid value for amount, must be a number")
+
+    # create new transaction
+    new_transaction = Transaction(
+        title=data["title"],
+        type=type,
+        sub_category=sub_category,
+        amount=float(data["amount"]),
+        description=data.get("description", "")
+    )
+    new_transaction.user_id = current_user.id
+
+    # add new_transaction to the database
+    db.session.add(new_transaction)
+    db.session.commit()
+
+    return jsonify({
+        "status": "success",
+        "message": "Transaction Created Successfully",
+        "Transaction": new_transaction.to_dict()
+    }), 201
